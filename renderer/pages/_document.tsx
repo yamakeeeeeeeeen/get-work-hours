5716acad52e3754561d7fe44ec06cddbd6e4d9a7
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-14 23:48:46 +0900

    chore: electron security warning disable setting

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -8,7 +8,6 @@ export default class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
         <body>
