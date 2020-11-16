0407f70a2f78d8b3e5b59109fe38ce2568995008
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-17 02:52:52 +0900

    feat: add react debug extensions

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,40 +1,49 @@
 // Native
-import { join } from 'path'
-import { format } from 'url'
+import { join } from "path";
+import { format } from "url";
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron'
-import isDev from 'electron-is-dev'
-import prepareNext from 'electron-next'
+import { BrowserWindow, app, ipcMain, IpcMainEvent } from "electron";
+import isDev from "electron-is-dev";
+import prepareNext from "electron-next";
 
 // Prepare the renderer once the app is ready
-app.on('ready', async () => {
-  await prepareNext('./renderer')
+app.on("ready", async () => {
+  await prepareNext("./renderer");
 
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: false,
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
     },
-  })
+  });
 
   const url = isDev
-    ? 'http://localhost:8000/'
+    ? "http://localhost:8000/"
     : format({
-        pathname: join(__dirname, '../renderer/out/index.html'),
-        protocol: 'file:',
+        pathname: join(__dirname, "../renderer/out/index.html"),
+        protocol: "file:",
         slashes: true,
-      })
+      });
 
-  mainWindow.loadURL(url)
-})
+  if (isDev) {
+    import("electron-devtools-installer").then((module) => {
+      module
+        .default([module.REACT_DEVELOPER_TOOLS, module.REDUX_DEVTOOLS])
+        .then((name) => console.log(`Added Extension: ${name}`))
+        .catch((err) => console.log("An error occurred: ", err));
+    });
+  }
+
+  mainWindow.loadURL(url);
+});
 
 // Quit the app once all windows are closed
-app.on('window-all-closed', app.quit)
+app.on("window-all-closed", app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  event.sender.send('message', message)
-})
+ipcMain.on("message", (event: IpcMainEvent, message: any) => {
+  event.sender.send("message", message);
+});
