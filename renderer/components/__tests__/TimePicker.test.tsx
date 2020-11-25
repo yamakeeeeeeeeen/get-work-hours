18e95a722269e6ca042fdb8ce328c4d60895b337
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-26 01:30:01 +0900

    refactor: TimePicker test

--- renderer/components/__tests__/TimePicker.test.tsx
+++ renderer/components/__tests__/TimePicker.test.tsx
@@ -1,8 +1,12 @@
+import { shallow } from 'enzyme';
 import { TimePicker } from '~/components/TimePicker';
-import { snapshot } from '~/test/utils';
+import toJson from 'enzyme-to-json';
 
+const wrapper = shallow(<TimePicker />);
+
+// NOTE: これ以上のテストは必要ない
 describe('<TimePicker />', () => {
   it('snapshot', () => {
-    snapshot(<TimePicker />);
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
