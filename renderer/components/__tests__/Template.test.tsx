a9a9be5eb27f7927bdea8f733228d8fe4eab088a
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-26 01:25:46 +0900

    feat: add Template test items

--- renderer/components/__tests__/Template.test.tsx
+++ renderer/components/__tests__/Template.test.tsx
@@ -1,16 +1,32 @@
+import { shallow } from 'enzyme';
 import { Props, Template } from '~/components/Template';
-import { snapshot } from '~/test/utils';
+import toJson from 'enzyme-to-json';
 
 const props: Pick<Props, 'title'> = {
   title: 'title: Template test',
 };
+const childrenText = 'children: Template test';
+const wrapper = shallow(
+  <Template {...props}>
+    <p>{childrenText}</p>
+  </Template>,
+);
 
 describe('<Template />', () => {
   it('snapshot', () => {
-    snapshot(
-      <Template {...props}>
-        <p>children: Template test</p>
-      </Template>,
-    );
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('children should be displayed as "children: Template test"', () => {
+    expect(wrapper.find('p').text()).toBe(childrenText);
+  });
+
+  it('title should be displayed as "title: Template test"', () => {
+    expect(wrapper.find('title').text()).toBe(props.title);
+  });
+
+  it('title should be displayed default', () => {
+    const withoutTitlePropsWrapper = shallow(<Template>without title props</Template>);
+    expect(withoutTitlePropsWrapper.find('title').text()).toBe('This is the default title');
   });
 });
